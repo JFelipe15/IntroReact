8c4a2ba0b15c4f44d27d1b3c2d0c629cc253441a
JFelipe15 <jfm1594@gmail.com>
2022-10-29 00:45:21 -0500

    Links entre plantillas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import logoCargado from './logo.svg';
 import './css/App.css';
+import Home from "./components/home";
+import Navbar from './components/navbar';
+import Ruta2 from "./components/Ruta2";
+import {Route, Link, Routes} from "react-router-dom";
 
+
+function Rutax(props){
+  return(
+    <div style={props.objCss}> 
+        <h2>Hola probando OBJETO css</h2>
+    </div>
+  );
+
+}
 
 //Componente App 
 function App() {
+
+  let objCss = {
+    border: "3px solid red",
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Navbar/>
+        <ul>
+            <li>
+              <Link to = {"/ruta2"}>Ir a ruta 2</Link>
+            </li>
+            <li><Link to = {"/"}>Ir a Home</Link></li>
+            <li><Link  to = {"/rutax"}>Ir a Objeto Css</Link></li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home logo = {logoCargado}/>}/>
+          <Route path="/rutax" element={<Rutax objCss = {objCss}/>}/>
+          <Route path="/ruta2" element={<Ruta2/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Ruta2.jsx
@@ -0,0 +1,9 @@
+function Ruta2(){
+    return(
+        <div>
+            <h2>Esta es la ruta 2, fijemonos que no estamos cargando la pagina</h2>
+        </div>
+    );
+}
+
+export default Ruta2;
